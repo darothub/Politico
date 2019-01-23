f714e7b99c0fde7a61e2da8398d8dc50db1834b1
darothub <darotudeen@gmail.com>
2019-01-23 08:15:14 +0100

    signin and sign up page fit to page

--- UI/js/login.js
+++ UI/js/login.js
@@ -1,28 +1,31 @@
-let signIn = document.getElementById("signin-link")
-let signUp = document.getElementById("signup-link")
-
-const openLoginPage=()=>{
-    signIn.addEventListener('click', (e)=>{
-        let signInBackground = document.getElementById("signin-form-background");
-    
-        if(signInBackground.style.display === "block"){
-            signInBackground.style.display = "none"
-            return false
-        }
-        signInBackground.style.display = "block";
-        e.preventDefault()
-    });
-    
+const showSignUp=()=>{
+    let form2 = document.getElementById("form-wrap2")
+    let form1 = document.getElementById("form-wrap1")
+    let signinTitle = document.getElementById("signin-title")
+    if(form2.style.display === "block" ){
+        signinTitle.textContent = "Sign in here"
+        form2.style.display = "none"
+        form1.style.display = "block"
+        return false
+    }
+    signinTitle.textContent = "Sign up here"
+    form2.style.display = "block";
+    form1.style.display = "none";
+    return false
 }
-const openSignupPage=()=>{
-    signUp.addEventListener('click', (e)=>{
-        let signInBackground = document.getElementById("signin-form-background");
-    
-        if(signInBackground.style.display === "block"){
-            signInBackground.style.display = "none"
-            return false
-        }
-        signInBackground.style.display = "block";
-        e.preventDefault()
-    });
+
+const showSignin=()=>{
+    let form2 = document.getElementById("form-wrap2")
+    let form1 = document.getElementById("form-wrap1")
+    let signinTitle = document.getElementById("signin-title")
+    if(form1.style.display === "block"){
+        signinTitle.textContent = "Sign up here"
+        form1.style.display = "none"
+        form2.style.display = "block"
+        return false
+    }
+    signinTitle.textContent = "Sign in here"
+    form1.style.display = "block";
+    form2.style.display = "none"
+    return false
 }
