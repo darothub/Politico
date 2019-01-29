569a0fd10b1354c9a64214fd1c9581659d2ef40a
darothub <darotudeen@gmail.com>
2019-01-29 08:56:34 +0100

    restructuring and refactoring-feedback

--- server/app/app.js
+++ server/app/app.js
@@ -31,9 +31,8 @@ app.use((req, res, next) => {
 });
 
 app.use((error, req, res, next) => {
-  res.status(error.status || 500);
-  res.json({
-    status: 500,
+  res.status(error.status || 500).json({
+    status: error.status,
     message: error.message,
   });
   next();
