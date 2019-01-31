c2113d8e1ee67d45b7111162df85e0e73811ebfb
darothub <darotudeen@gmail.com>
2019-01-31 13:56:20 +0100

    user can sign in

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -8,12 +8,17 @@ const router = express.Router();
 const versionedapi = '/auth/';
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 router.post(`${versionedapi}signup`, User.signup);
 router.post(`${versionedapi}signin`, User.signin);
 =======
 
 router.post(`${versionedapi}signup`, User.signup);
 >>>>>>> user can signup
+=======
+router.post(`${versionedapi}signup`, User.signup);
+router.post(`${versionedapi}signin`, User.signin);
+>>>>>>> user can sign in
 
 
 export default router;
