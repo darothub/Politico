af72e20c95c67db4cb75b4c0460cf0e67f8f5d30
darothub <darotudeen@gmail.com>
2019-01-31 13:26:22 +0100

    user can signup

--- server/model/database.js
+++ server/model/database.js
@@ -8,7 +8,7 @@ const pool = new pg.Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
-  password: process.env.DB_pass,
+  password: process.env.DB_PASS,
   port: process.env.DB_PORT,
   max: 10,
   idleTimeoutMillis: 3000,
