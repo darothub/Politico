25063b08621af9682b3463b9b96fb055c9736f15
darothub <darotudeen@gmail.com>
2019-02-01 08:51:36 +0100

    user can register for an office

--- server/model/table.sql
+++ server/model/table.sql
@@ -10,27 +10,24 @@ CREATE TABLE users(
     phone_number VARCHAR NOT NULL,
     password VARCHAR NOT NULL,
     passport_url VARCHAR NOT NULL,
-    isAdmin BOOLEAN NULL
+    is_admin BOOLEAN DEFAULT false
 );
 CREATE TABLE parties(
-    ID SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name VARCHAR NOT NULL,
     hqAddress VARCHAR NOT NULL,
     logoUrl VARCHAR NOT NULL
 );
 CREATE TABLE offices(
-    ID SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name VARCHAR NOT NULL,
     type VARCHAR NOT NULL
 );
 
 CREATE TABLE candidates(
-    ID SERIAL PRIMARY KEY,
-    office INT NOT NULL,
-    party INT NOT NULL,
-    candidate INT NOT NULL
+    id SERIAL REFERENCES users(id),
+    office INTEGER NOT NULL REFERENCES offices(id),
+    party INTEGER NOT NULL,
+    candidate INTEGER NOT NULL
 );
 
--- INSERT INTO users VALUES 
---     ('darot', 'darotudeen@gmail.com', 'okbakassi'),
---     ('amoke', 'amoke@gmail.com', 'alonge');
