f79f36f462e6d07ff8a4b688c9356a6a26874d0f
darothub <darotudeen@gmail.com>
2019-02-03 10:38:02 +0100

    test setup

--- server/controller/partyController.js
+++ server/controller/partyController.js
@@ -1,27 +1,47 @@
+import jwt from 'jsonwebtoken';
+import pool from '../model/database';
 import parties from '../db/dummy';
-
 import Helper from '../helper/util';
 
 class Party {
   static createParty(req, res) {
-    const request = req.body;
-    if (!Helper.isParty(request)) {
+    const decoded = jwt.verify(req.token, process.env.SECRET_KEY);
+    const { name, hqAddress, logoUrl } = req.body;
+    const selQuery = {
+      text: 'SELECT * FROM parties WHERE name=$1',
+      values: [name],
+    };
+    const insQuery = {
+      text: 'INSERT INTO parties(name, hqAddress, logoUrl) VALUES($1, $2, $3) RETURNING *',
+      values: [name, hqAddress, logoUrl],
+    };
+
+    if (!Helper.isParty(req.body)) {
       res.status(400).json({
         status: 400,
         error: 'Invalid name/hqAddress/logoUrl',
       });
     }
-    const newParty = {
-      id: parties.length + 1,
-      name: request.name,
-      hqAddress: request.hqAddress,
-      logoUrl: request.logoUrl,
-    };
-    parties.push(newParty);
-    return res.status(201).json({
-      status: 201,
-      data: newParty,
-    });
+    return pool.query(selQuery)
+      .then((party) => {
+        if (party.rowCount === 1) {
+          return res.status(409).send({
+            status: 409,
+            message: 'Party exist already',
+          });
+        }
+        if (decoded.is_admin !== true) {
+          return res.status(401).json({
+            status: 401,
+            message: 'You need an admin right to perform this task',
+          });
+        }
+        return pool.query(insQuery)
+          .then(newParty => res.status(201).json({
+            status: 201,
+            data: newParty.rows[0],
+          }));
+      });
   }
 
   static getAllParties(req, res) {
