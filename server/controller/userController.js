28483f4671d0dc65a0c8778230ce78af46dbead7
darothub <darotudeen@gmail.com>
2019-02-07 17:10:57 +0100

    remove email from signin

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -10,23 +10,20 @@ import Helper from '../helper/util';
 
 dotenv.config();
 
+
 class User {
   static signup(req, res) {
     const {
 
-      userId, firstName, lastName, otherName, email, phoneNumber, passportUrl, password,
+      firstName, lastName, otherName, email, phoneNumber, passportUrl, password,
     } = req.body;
     const selQuery1 = {
       text: 'SELECT * FROM users WHERE email=$1',
       values: [email],
     };
-    const selQuery2 = {
-      text: 'SELECT * FROM users WHERE user_ids =$1',
-      values: [userId],
-    };
     const insQuery = {
-      text: 'INSERT INTO users(user_ids, first_name, last_name, other_name, email, phone_number, passport_url, password) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *',
-      values: [userId, firstName, lastName, otherName, email, phoneNumber, passportUrl,
+      text: 'INSERT INTO users(first_name, last_name, other_name, email, phone_number, passport_url, password) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *',
+      values: [firstName, lastName, otherName, email, phoneNumber, passportUrl,
         bcrypt.hashSync(password, 10)],
     };
     if (!Helper.isValidEmailPassword(req.body)) {
@@ -56,38 +53,28 @@ class User {
             message: 'Email address has already been used',
           });
         }
-        return pool.query(selQuery2)
-          .then((user) => {
-            if (user.rowCount === 1) {
-              return res.status(409).send({
-                status: 409,
-                message: 'User ID is invalid',
+        return pool.query(insQuery)
+          .then((userData) => {
+            if (userData) {
+              const token = jwt.sign({
+                id: userData.rows[0].user_id,
+                isAdmin: userData.rows[0].is_admin,
+              }, process.env.SECRET_KEY, {
+                expiresIn: '365d',
+              });
+              res.status(201).json({
+                status: 201,
+                data: {
+                  token,
+                  user_ids: userData.rows[0].user_id,
+                  firstName: userData.rows[0].first_name,
+                  lastName: userData.rows[0].last_name,
+                  otherName: userData.rows[0].other_name,
+                  passport: userData.rows[0].passport_url,
+                  email: userData.rows[0].email,
+                },
               });
             }
-            return pool.query(insQuery)
-              .then((userData) => {
-                if (userData) {
-                  const token = jwt.sign({
-                    id: userData.rows[0].user_id,
-                    isAdmin: userData.rows[0].is_admin,
-                  }, process.env.SECRET_KEY, {
-                    expiresIn: '365d',
-                  });
-                  res.status(201).json({
-                    status: 201,
-                    data: {
-                      token,
-                      user_ids: userData.rows[0].user_id,
-                      firstName: userData.rows[0].first_name,
-                      lastName: userData.rows[0].last_name,
-                      otherName: userData.rows[0].other_name,
-                      passport: userData.rows[0].passport_url,
-                      email: userData.rows[0].email,
-                    },
-                  });
-                }
-              })
-              .catch(e => res.send(e));
           })
           .catch(e => res.send(e));
       })
@@ -117,15 +104,20 @@ class User {
         if (bcrypt.compareSync(req.body.password, data.rows[0].password)) {
           const token = jwt.sign({
             id: data.rows[0].id,
-            email: data.rows[0].email,
-
             is_admin: data.rows[0].is_admin,
           }, process.env.SECRET_KEY, {
             expiresIn: '365d',
           });
           return res.status(200).json({
             status: 200,
-            data: { token, user: data.rows[0].email },
+            data: {
+              token,
+              userId: data.rows[0].id,
+              firstName: data.rows[0].first_name,
+              lastName: data.rows[0].last_name,
+              otherName: data.rows[0].other_name,
+              email: data.rows[0].email,
+            },
             message: 'You have successfully signed in',
           });
         }
--- server/model/database.js
+++ server/model/database.js
@@ -4,23 +4,24 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const {
+  DB_USER, DB_NAME, DB_PASS, DB_PORT2, DB_PORT, TESTDB_NAME,
+} = process.env;
 
 let connection;
 const string = {
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT2,
+  user: DB_USER,
+  database: DB_NAME,
+  password: DB_PASS,
+  port: DB_PORT,
   max: 50,
   idleTimeoutMillis: 3000,
 };
 const stringTest = {
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME2,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT2,
+  user: DB_USER,
+  database: TESTDB_NAME,
+  password: DB_PASS,
+  port: DB_PORT2,
   max: 50,
   idleTimeoutMillis: 3000,
 };
