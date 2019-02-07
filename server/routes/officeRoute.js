d805a8ebca38c27556cd1bbe2ce1ce6e7f8f1835
darothub <darotudeen@gmail.com>
2019-02-08 00:18:41 +0100

    get office by id

--- server/routes/officeRoute.js
+++ server/routes/officeRoute.js
@@ -12,6 +12,7 @@ const versionedapi2 = '/office/:userId/';
 router.route(`${versionedapi}`)
   .post(checkAuth, Office.addOffice);
 router.get(`${versionedapi}`, Office.getAllOffices);
+router.get(`${versionedapi}:id`, Office.getOfficeById);
 router.post(`${versionedapi2}register`, checkAuth, Office.addNewCandidate);
 router.get('/office/:officeId/result', Office.getOfficeResult);
 
