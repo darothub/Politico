3cd4cae8aa739077ad459d85d2138f07acf0b139
darothub <darotudeen@gmail.com>
2019-04-29 08:03:53 +0100

    implementing party fetch

--- UI/fetch/party.js
+++ UI/fetch/party.js
@@ -1,10 +1,42 @@
-let partyBtn = document.getElementById('party-btn');
+// let partyBtn = document.getElementById('party-btn');
 
-const getParty =(e)=>{
-    fetch('https://polityco.herokuapp.com/api/v1/parties/')
-        .then((res)=>{return res.json()})
-        .then((data) => console.log(data))
-        .catch((err) => console.log(err))
+// const getParty =(e)=>{
+//     e.preventDefault();
+//     fetch('https://polityco.herokuapp.com/api/v1/parties/')
+//         .then((res)=>{return res.json()})
+//         .then((data) => console.log(data))
+//         .catch((err) => console.log(err))
+// }
+
+// partyBtn.addEventListener("click", getParty);
+
+
+
+let signUpBtn = document.querySelector('#enter');
+
+
+const postData = (e) => {
+    e.preventDefault();
+
+    // let partyname = document.querySelector("#partyname").value;
+    // let partyId = document.querySelector("#party-id").value;
+    // let hqAddress = document.querySelector("#addrss").value;
+
+    let formData = document.querySelector("#formData");
+
+
+    fetch(`https://polityco.herokuapp.com/api/v1/parties`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+        // "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new FormData(formData)
+    })
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(err => console.log(err));
 }
 
-partyBtn.addEventListener('click', getParty)
+signUpBtn.addEventListener("click", postData);
+
